b4973c65259c16932877817fb91de6fe11a03ff8
mukulBillore <mukulbillore88@gmail.com>
2022-01-04 18:16:20 +0530

    Added : added the functionality of Last name validation

--- src/com/bridgeLab/regex/MainClass.java
+++ src/com/bridgeLab/regex/MainClass.java
@@ -7,10 +7,15 @@ public class MainClass {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String re1 = "[A-Z][a-z]{3}[a-z]*";
-		RegexPract uc1 = new RegexPract(re1);
+		RegexPract firstName = new RegexPract(re1);
 		System.out.println("Enter the first name to check");	
 		String input1=sc.nextLine();
-		System.out.println(uc1.patternChecker(input1));
+		System.out.println(firstName.patternChecker(input1));
+		String re2 = "[A-Z][a-z]{3}[a-z]*";
+		RegexPract lastName = new RegexPract(re1);
+		System.out.println("Enter the last name to check");	
+		String input2=sc.nextLine();
+		System.out.println(lastName.patternChecker(input2));
 	}
 
 }
