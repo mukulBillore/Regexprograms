8a2a61d811defd5b8649b4d534e6b167dc169ea5
mukulBillore <mukulbillore88@gmail.com>
2022-01-04 18:48:31 +0530

    Added : added the functionality of email validation

--- src/com/bridgeLab/regex/MainClass.java
+++ src/com/bridgeLab/regex/MainClass.java
@@ -6,16 +6,24 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		/*
 		String re1 = "[A-Z][a-z]{3}[a-z]*";
 		RegexPract firstName = new RegexPract(re1);
 		System.out.println("Enter the first name to check");	
 		String input1=sc.nextLine();
 		System.out.println(firstName.patternChecker(input1));
 		String re2 = "[A-Z][a-z]{3}[a-z]*";
-		RegexPract lastName = new RegexPract(re1);
+		RegexPract lastName = new RegexPract(re2);
 		System.out.println("Enter the last name to check");	
 		String input2=sc.nextLine();
 		System.out.println(lastName.patternChecker(input2));
+	*/  int a;
+		String re3 = "^[a-zA-Z0-9]*(\\.[a-z0-9]+)?@bl\\.co(\\.in)?$";
+		RegexPract email = new RegexPract(re3);
+		System.out.println("Enter the mail to check");	
+		String input3=sc.nextLine();
+		System.out.println(email.patternChecker(input3));
+	
 	}
 
 }
